350a9edb4a3b515118d04a02d77a12b7eabe741a
Christian Fobel <christian@fobel.net>
2017-07-31 21:35:46 -0400

    Remove memory alloc methods, linked-list to save PROGMEM

--- src/Node.h
+++ src/Node.h
@@ -65,9 +65,6 @@ public:
 
   uint8_t buffer_[BUFFER_SIZE];
 
-  LinkedList<uint32_t> allocations_;
-  LinkedList<uint32_t> aligned_allocations_;
-
   Node() : BaseNode(),
            BaseNodeConfig<config_t>(mr_box_peripheral_board_Config_fields),
            BaseNodeState<state_t>(mr_box_peripheral_board_State_fields),
@@ -127,58 +124,6 @@ public:
   uint16_t analog_input_to_digital_pin(uint16_t pin) { return analogInputToDigitalPin(pin); }
   uint16_t digital_pin_has_pwm(uint16_t pin) { return digitalPinHasPWM(pin); }
   uint16_t digital_pin_to_interrupt(uint16_t pin) { return digitalPinToInterrupt(pin); }
-
-  // ##########################################################################
-  // # Mutator methods
-  uint32_t mem_alloc(uint32_t size) {
-    uint32_t address = (uint32_t)malloc(size);
-    // Save to list of allocations for memory management.
-    allocations_.add(address);
-    return address;
-  }
-  uint32_t mem_aligned_alloc(uint32_t alignment, uint32_t size) {
-    uint32_t address = (uint32_t)aligned_malloc(alignment, size);
-    // Save to list of allocations for memory management.
-    aligned_allocations_.add(address);
-    return address;
-  }
-  uint32_t mem_aligned_alloc_and_set(uint32_t alignment, UInt8Array data) {
-    // Allocate aligned memory.
-    const uint32_t address = mem_aligned_alloc(alignment, data.length);
-    if (!address) { return 0; }
-    // Copy data to allocated memory.
-    mem_cpy_host_to_device(address, data);
-    return address;
-  }
-  void mem_aligned_free(uint32_t address) {
-    for (int i = 0; i < aligned_allocations_.size(); i++) {
-      if (aligned_allocations_.get(i) == address) {
-        aligned_allocations_.remove(i);
-      }
-    }
-    aligned_free((void *)address);
-  }
-  void mem_cpy_host_to_device(uint32_t address, UInt8Array data) {
-    memcpy((uint8_t *)address, data.data, data.length);
-  }
-  void mem_fill_uint8(uint32_t address, uint8_t value, uint32_t size) {
-    mem_fill((uint8_t *)address, value, size);
-  }
-  void mem_fill_uint16(uint32_t address, uint16_t value, uint32_t size) {
-    mem_fill((uint16_t *)address, value, size);
-  }
-  void mem_fill_uint32(uint32_t address, uint32_t value, uint32_t size) {
-    mem_fill((uint32_t *)address, value, size);
-  }
-  void mem_fill_float(uint32_t address, float value, uint32_t size) {
-    mem_fill((float *)address, value, size);
-  }
-  void mem_free(uint32_t address) {
-    for (int i = 0; i < allocations_.size(); i++) {
-      if (allocations_.get(i) == address) { allocations_.remove(i); }
-    }
-    free((void *)address);
-  }
 };
 }  // namespace mr_box_peripheral_board
 
