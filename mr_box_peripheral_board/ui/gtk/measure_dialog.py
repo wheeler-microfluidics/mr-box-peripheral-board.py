cfc15bbf7c3d16ecbc3d2b700ab50c5cb7bc916f
Alexandros Sklavounos <alexandros.sklavounos@mail.utoronto.ca>
2017-08-31 00:37:45 +0100

    PMT IVC resistor switched to 300 kOhm & ADC Baseline shift

--- mr_box_peripheral_board/ui/gtk/measure_dialog.py
+++ mr_box_peripheral_board/ui/gtk/measure_dialog.py
@@ -191,8 +191,8 @@ def adc_data_func_factory(proxy, delta_t=dt.timedelta(seconds=1), adc_rate=1 ):
                                        duration_s=delta_t.total_seconds())
                 #Convert data to Voltage, 24bit ADC with Vref = 3.0 V
                 data_i /=  ((2 ** 24 - 1)/(3.0/adc_dgain))
-                #Convert Voltage to Current, 30kOhm Resistor
-                data_i /= 30e3
+                #Convert Voltage to Current, 300kOhm Resistor
+                data_i /= 300e3
                 # Set name to display units.
                 data_i.name = 'Current (A)'
                 data.append(data_i)
